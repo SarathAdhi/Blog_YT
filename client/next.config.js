56fa252bb546148b4a9566764f893e5782054d50
SarathAdhi <s.sarathadhithya@gmail.com>
2022-06-09 10:29:53 +0530

    bug fixed

--- client/next.config.js
+++ client/next.config.js
@@ -2,18 +2,22 @@ module.exports = (phase, { defaultConfig }) => {
   return {
     ...defaultConfig,
     images: {
-      domains: ['ipfs.infura.io'],
+      domains: ["ipfs.infura.io"],
+    },
+    publicRuntimeConfig: {
+      // Will be available on both server and client
+      staticFolder: "/static",
     },
     webpack: (config) => {
       config.resolve = {
         ...config.resolve,
         fallback: {
-          "fs": false,
-          "path": false,
-          "os": false,
-        }
-      }
-      return config
+          fs: false,
+          path: false,
+          os: false,
+        },
+      };
+      return config;
     },
-  }
-}
+  };
+};
